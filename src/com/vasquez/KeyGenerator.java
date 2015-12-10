78de5dadd1e3b97c53e145869e41302d967c0456
Jonathan Vasquez <jvasquez1011@gmail.com>
2015-12-10 14:52:20 -0500

    Now with O(1) solution!

--- src/com/vasquez/KeyGenerator.java
+++ src/com/vasquez/KeyGenerator.java
@@ -15,101 +15,70 @@
 package com.vasquez;
 
 public class KeyGenerator {
-	public KeyGenerator()
-	{
+	public KeyGenerator() {
 		serialNumber = new int[bluetoothAddressMaxSections];
+		solution = new int[bluetoothAddressMaxSections];
 	}
-	
-	public KeyGenerator(String Isn)
-	{
+
+	public KeyGenerator(String isn) {
 		this();
-		PopulateSerialNumber(Isn);
+		PopulateSerialNumber(isn);
+		this.isn = getIntegersToHexAsString(serialNumber);
 	}
-	
-	private void PopulateSerialNumber(String Isn)
-	{
-		String[] splitted = Isn.split(",");
-		
-		for(int i = 0; i < bluetoothAddressMaxSections; i++)
-		{
+
+	private void PopulateSerialNumber(String isn) {
+		String[] splitted = isn.split(",");
+
+		for (int i = 0; i < bluetoothAddressMaxSections; i++) {
 			serialNumber[i] = Integer.valueOf(String.valueOf(splitted[i]), 16);
 		}
 	}
-	
-	public String FindKey()
-	{
-		int value1 = 0; // 0
-		int value2 = 0;
-	    int value3 = 0; // 2
-	    int value4 = 0;
-	    int value5 = 0; // 4
-	    int checksum = 0; //value 6 aka checksum
-	    
-	    System.out.println("Calculating key for " + getIntegersToHexAsString(serialNumber[0], serialNumber[1], serialNumber[2], serialNumber[3], serialNumber[4], serialNumber[5]) + " ...\n");
-		System.out.println("Please be patient. This can take a long time.\n");
 
-		for (checksum = 0; checksum < 255; checksum++) {
-			for (value5 = 0; value5 < 255; value5++) {
-				for (value4 = 0; value4 < 255; value4++) {
-					for (value3 = 0; value3 < 255; value3++) {
-						for (value2 = 0; value2 < 255; value2++) {
-							for (value1 = 0; value1 < 255; value1++) {
-								int potential = value1 + value2 + value3 + value4 + value5;
-								int checksumed = potential & 0xff;
-								
-								//We got a potential checksum match
-								if (checksumed == checksum) {
-									//Now see if it qualifies the other requirements [0, 2, 4] = 255
-									
-									int test = doesValueMatchCorrectlyWithAnyInDeviceSerial(value1);
-									if (test == -1) continue;
-									
-									test = doesValueMatchCorrectlyWithAnyInDeviceSerial(value3);
-									if (test == -1) continue;
-									
-									test = doesValueMatchCorrectlyWithAnyInDeviceSerial(value5);
-									if (test == -1) continue;
-									
-									return getIntegersToHexAsString(value1,value2,value3,value4,value5,checksum);
-								}
-							}
-						}
-					}
-				}
+	public String FindKey() {
+		solution[0] = 255 - serialNumber[0];
+		solution[2] = 255 - serialNumber[2];
+		solution[4] = 255 - serialNumber[4];
+		solution[5] = (solution[0] + solution[2] + solution[4]) & 0xFF;
+
+		System.out.println("Device : " + isn);
+
+		for (int i = 0; i < bluetoothAddressMaxSections - 1; i += 2) {
+			if (!doesValueMatchCorrectlyWithAnyInDeviceSerial(solution[i])) {
+				// No Key Found
+				return null;
 			}
 		}
-		
-		// No Key Found
-		return null;
-	}
-		
-	// If this value plus the sum of any serial number add up to 255, then we are good to go.
-	private int doesValueMatchCorrectlyWithAnyInDeviceSerial(int value)
-	{
-	    for(int i = 0; i < bluetoothAddressMaxSections; i++)
-	    {
-	        if(value + serialNumber[i] == 255) {
-	            return value;
-	        }
-	    }
-	    return -1;
+
+		return getIntegersToHexAsString(solution);
 	}
 
-	private String getIntegersToHexAsString(int value1, int value2, int value3, int value4, int value5, int checksum)
-	{
-	    String solutionAsHex = "";
-	    String seperator = ":";
-	    
-	    solutionAsHex = String.format("%02x", value1);
-	    solutionAsHex = solutionAsHex + seperator + String.format("%02x", value2);
-	    solutionAsHex = solutionAsHex + seperator + String.format("%02x", value3);
-	    solutionAsHex = solutionAsHex + seperator + String.format("%02x", value4);
-	    solutionAsHex = solutionAsHex + seperator + String.format("%02x", value5);
-	    solutionAsHex = solutionAsHex + seperator + String.format("%02x", checksum);
-	    
-	    return solutionAsHex.toUpperCase();
+	// If this value plus the sum of any serial number add up to 255, then we
+	// are good to go.
+	private boolean doesValueMatchCorrectlyWithAnyInDeviceSerial(int value) {
+		for (int i = 0; i < bluetoothAddressMaxSections; i++) {
+			if (value + serialNumber[i] == 255) {
+				return true;
+			}
+		}
+		return false;
 	}
-	
+
+	private String getIntegersToHexAsString(int[] values) {
+		StringBuilder solutionAsHex = new StringBuilder(values.length * 3);
+		String seperator = ":";
+
+		for (int i = 0; i < values.length; i++) {
+			solutionAsHex.append(String.format("%02x", values[i]));
+			if (i < values.length - 1) {
+				solutionAsHex.append(seperator);
+			}
+		}
+
+		return solutionAsHex.toString().toUpperCase();
+	}
+
 	private int[] serialNumber;
+	private int[] solution;
 	private final int bluetoothAddressMaxSections = 6;
+	private String isn;
 }
--- src/com/vasquez/Main.java
+++ src/com/vasquez/Main.java
@@ -18,33 +18,31 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("King Song 4.0 ISN Key Generator - 1.0.0");
+		System.out.println("King Song 4.0 ISN Key Generator - 1.0.1");
 		System.out.println("By Jonathan Vasquez (fearedbliss) <jvasquez1011@gmail.com>");
 		System.out.println("Released under the Apache License 2.0\n");
-		
-		System.out.print("Please enter your ISN number [Example: 11,22,33,44,55,66]: ");
-		
+
+		System.out.print("ISN # [Example: 11,22,33,44,55,66]: ");
+
 		Scanner scanner = new Scanner(System.in);
 		String Isn = scanner.next();
-		
+
 		System.out.println("");
-		
+
 		try {
 			KeyGenerator keyGen = new KeyGenerator(Isn);
 			String result = keyGen.FindKey();
-		
-			if(result != null) {
-				System.out.println("Key Found: " + result + "\n");
-				System.out.println("Enjoy and Ride Safely!\n");
+
+			result = (result != null) ? result : "Not Found";
+			System.out.println("Key    : " + result + "\n");
+
+			if (result != null) {
+				System.out.println("Enjoy and Ride Safely!");
 			}
-			else {
-				System.out.println("No Key Found!\n");
-			}	
-		}
-		catch(Exception ex) {
+		} catch (Exception ex) {
 			System.out.println("Error parsing the ISN number. Make sure you typed it correctly.");
 		}
-		
+
 		scanner.close();
 	}
 }
